7faca7d49001eae7b141b135dfae2632e31bcff6
Qasim Bin Saeed <qasimsaeed590@gmail.com>
2024-04-03 02:21:24 +0500

    Async Function added

--- 9 - Hooks Use/my-react-app/src/App.jsx
+++ 9 - Hooks Use/my-react-app/src/App.jsx
@@ -5,20 +5,27 @@ import Card from "./Card.jsx";
 
 
 
+
 function App() {
   const [posts, setPosts] = useState([])
-
+  const fetchAPI = async() => {
+    try {
+      let response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      let data = await response.json();
+      setPosts(data); // Update the state with the fetched data
+      console.log(data)
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  }
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => setPosts(data))
-      .catch(error => console.error('Error fetching data:', error));
+    fetchAPI()
   }, [])
   return (
     <>
       <Navbar />
-      <div className="container mx-auto mt-8">
+      <div className="container mx-auto mt-8 p-4">
         <h1 className="text-3xl font-bold mb-4 text-white">Posts</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {posts.map(post => (
